4b1488244a0e91a8d6c9f5caa34cc369d8b7087e
longzhouLab <longzhou@doupai.cc>
2018-11-11 16:42:04 +0800

    add

---
+++ 两数之和.js
@@ -0,0 +1,23 @@
+/**
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用
+ * 示例:
+ *  给定 nums = [2, 7, 11, 15], 
+ *  target = 9
+ *  因为 nums[0] + nums[1] = 2 + 7 = 9
+ *  所以返回 [0, 1]
+ */
+
+const nums = [2, 7, 11, 15],
+      target = 9
+
+var twoSum = function(nums, target) {
+    for (let i = 0, len = nums.length; i < len; i++) {
+      for (let j = i + 1, len = nums.length; j < len; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j]
+        }
+      }
+    }
+}
+
